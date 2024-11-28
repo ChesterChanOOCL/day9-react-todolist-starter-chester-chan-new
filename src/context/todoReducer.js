11469fe836e89317ffabff3d38553d03ad2de9ba
CHANCH10 <chester.chan@oocl.com>
2024-11-29 00:30:46 +0800

    feat: todoReducer.js

--- src/context/todoReducer.js
+++ src/context/todoReducer.js
@@ -1,8 +1,31 @@
+import React, {  useReducer } from 'react';
+import {TodoContext} from "../App";
 export const initialState = [
-  {id: Date.now(), text: "the first todo", done: false},
-  {id: Date.now(), text: "the second todo", done: false},
+  { id: Date.now(), text: "the first todo", done: false },
+  { id: Date.now() + 1, text: "the second todo", done: false },
 ];
 
 export const todoReducer = (state, action) => {
-  return state;
+  switch (action.type) {
+    case "ADD":
+      return [...state, { id: Date.now(), text: action.payload, done: false }];
+    case "TOGGLE":
+      return state.map((todo) =>
+          todo.id === action.payload ? { ...todo, done: !todo.done } : todo
+      );
+    case "REMOVE":
+      return state.filter((todo) => todo.id !== action.payload);
+    default:
+      return state;
+  }
+};
+
+export const TodoListProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(todoReducer, initialState);
+
+  return (
+      <TodoContext.Provider value={{ state, dispatch }}>
+        {children}
+      </TodoContext.Provider>
+  );
 };
