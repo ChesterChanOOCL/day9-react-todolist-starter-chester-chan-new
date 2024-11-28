9ba52adc07ff86fda7204bfd9d3e819781c567ab
CHANCH10 <chester.chan@oocl.com>
2024-11-29 00:29:55 +0800

    feat: TodoItem.jsx

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { TodoContext } from "../App";
+
+const TodoItem = ({ todo }) => {
+    const { dispatch } = useContext(TodoContext);
+
+    return (
+        <div className="todo-item-wrapper">
+            <div
+                className={`todo-text ${todo.done ? "done" : ""}`}
+                onClick={() => dispatch({ type: 'TOGGLE', payload: todo.id })}
+            >
+                {todo.text}
+            </div>
+            <button className="crossButton" onClick={() => dispatch({ type: 'REMOVE', payload: todo.id })}>
+                X
+            </button>
+        </div>
+    );
+};
+
+export default TodoItem;
