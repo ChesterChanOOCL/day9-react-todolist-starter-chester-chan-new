311af662ce7dae0d8e0367002725088934631851
CHANCH10 <chester.chan@oocl.com>
2024-11-29 18:02:01 +0800

    feat: update pop up box

--- src/components/TodoGroup.jsx
+++ src/components/TodoGroup.jsx
@@ -3,17 +3,11 @@ import TodoItem from "./TodoItem";
 import {TodoContext} from "../App";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
-import "./TodoGroup.css";
-
-
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-
-
+import "./TodoGroup.css"
 const TodoGroup = () => {
-    const { state, dispatch } = useContext(TodoContext);
+    const { state } = useContext(TodoContext)
     const [currentPage, setCurrentPage] = useState(1);
     const itemsPerPage = 5;
-
     const handlePageChange = (event, value) => {
         setCurrentPage(value);
     };
@@ -22,53 +16,17 @@ const TodoGroup = () => {
     const indexOfFirstItem = indexOfLastItem - itemsPerPage;
     const currentItems = state.slice(indexOfFirstItem, indexOfLastItem);
 
-    const onDragEnd = (result) => {
-        if (!result.destination) return;
-
-        const reorderedItems = Array.from(state);
-        const [removed] = reorderedItems.splice(result.source.index, 1);
-        reorderedItems.splice(result.destination.index, 0, removed);
-
-        dispatch({ type: 'REORDER', payload: reorderedItems });
-    };
-
     return (
         <div>
             {state.length === 0 ? (
                 <span>Add an Item</span>
             ) : (
-                <DragDropContext onDragEnd={onDragEnd}>
-                    <Droppable droppableId="todo-list">
-                        {(provided) => (
-                            <div
-                                {...provided.droppableProps}
-                                ref={provided.innerRef}
-                            >
-                                {currentItems.map((todo, index) => (
-                                    <Draggable
-                                        key={todo.id}
-                                        draggableId={todo.id.toString()}
-                                        index={index}
-                                    >
-                                        {(provided) => (
-                                            <div
-                                                ref={provided.innerRef}
-                                                {...provided.draggableProps}
-                                                {...provided.dragHandleProps}
-                                            >
-                                                <TodoItem key={todo.id} todo={todo} />
-                                            </div>
-                                        )}
-                                    </Draggable>
-                                ))}
-                                {provided.placeholder}
-                            </div>
-                        )}
-                    </Droppable>
-                </DragDropContext>
+                currentItems.map((todo) => (
+                    <TodoItem key={todo.id} todo={todo}/>
+                ))
             )}
             <div className={"pagination-bar"}>
-                <Stack spacing={2}>
+                <Stack spacing={2} >
                     <Pagination
                         count={Math.ceil(state.length / itemsPerPage)}
                         page={currentPage}
@@ -79,6 +37,7 @@ const TodoGroup = () => {
             </div>
         </div>
     );
-};
+}
 
 export default TodoGroup;
+
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,24 +1,42 @@
-import { useContext } from "react";
-import { TodoContext } from "../App";
+import React, {useContext} from "react";
+import {TodoContext} from "../App";
 import "./TodoItem.css";
 import {removeTodoItem, toggleTodoItem} from "../api/todo";
 import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import Stack from '@mui/material/Stack';
-const TodoItem = ({ todo }) => {
-    const { dispatch } = useContext(TodoContext);
+
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+
+const TodoItem = ({todo}) => {
+    const {dispatch} = useContext(TodoContext);
 
     const handleToggle = () => {
         toggleTodoItem(todo.id).then(() => {
-            dispatch({ type: 'TOGGLE', payload: todo.id });
+            dispatch({type: 'TOGGLE', payload: todo.id});
         })
     }
     const handleRemove = () => {
-        removeTodoItem(todo.id).then(()=>{
-            dispatch({ type: 'REMOVE', payload: todo.id });
+        removeTodoItem(todo.id).then(() => {
+            dispatch({type: 'REMOVE', payload: todo.id});
         })
     }
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     return (
         <div className="todo-item-wrapper">
@@ -30,12 +48,34 @@ const TodoItem = ({ todo }) => {
             </div>
 
             <Stack direction="row" spacing={2}>
-                <Button onClick={handleRemove} sx={{"font-size":"10px"}} variant="outlined" startIcon={<DeleteIcon />}>
+                <Button onClick={handleRemove} sx={{"font-size": "10px"}} variant="outlined" startIcon={<DeleteIcon/>}>
                     Delete
                 </Button>
-                <Button sx={{"font-size":"10px"}} variant="contained" endIcon={<EditIcon />}>
+                <Button onClick={handleClickOpen} sx={{"font-size": "10px"}} variant="contained" endIcon={<EditIcon/>}>
                     Update
                 </Button>
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    PaperProps={{
+                        component: 'form',
+                    }}
+                >
+                    <DialogTitle>Edit </DialogTitle>
+                    <DialogContent>
+                        <TextField
+                            autoFocus
+                            required
+                            margin="dense"
+                            fullWidth
+                            variant="standard"
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose}>Cancel</Button>
+                        <Button type="submit">Confirm</Button>
+                    </DialogActions>
+                </Dialog>
             </Stack>
         </div>
     );
